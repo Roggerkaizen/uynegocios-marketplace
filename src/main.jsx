96ae9a731b25a6990a21ab000e4a0d9b190aaf19
uyu <kr.uyu33@gmail.com>
2025-07-28 14:07:02 -0300

    Projeto inicial funcional

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('app')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>Frontend funcionando com TailwindCSS!</div>
+    <App />
   </React.StrictMode>
 )
